f8927a984c422399ce8780815d4be75319825888
SynneK1337 <synnek1337@protonmail.com>
2019-08-27 03:02:18 +0200

    passwords are now being hashed :)

--- src/auth.py
+++ src/auth.py
@@ -6,16 +6,19 @@ class Auth():
     def __init__(self):
         pass
 
+    def generate_hash(self, input):
+        hash = hashlib.sha256()
+        hash.update(input.encode("utf-8"))
+        return hash.hexdigest()
+
     def generate_token(self, username):
         current_date = int(datetime.now().timestamp())
-        token = hashlib.sha256()
-        token.update(bytes(current_date))
-        token.update(bytes(username.encode("utf-8")))
-        return token.hexdigest()
+        return self.generate_hash(str(current_date) + username)
 
     def verify_token(self, token, expiration_date):
         return datetime.now() <= expiration_date
 
+
 if __name__ == "__main__":
     auth = Auth()
-    auth.generate_token("synnek")
+    print(auth.generate_token("synnek"))
